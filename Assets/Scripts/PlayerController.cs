6d7a984e2f9ce18ed52bd6363d3cc8ead876f530
Calvin Chan <ccha@tugo.com>
2017-12-03 22:28:10 -0800

    trying again

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int count;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		setCountText ();
+		winText.text = "";
+	}
+
+	//called before rendering a frame
+	void Update ()
+	{
+	}
+
+	//Called just before any physics calculations
+	void FixedUpdate ()
+	{
+//		float moveHorizontal = Input.GetAxis ("Horizontal");
+//		float moveVertical = Input.GetAxis ("Vertical");
+
+		float moveHorizontal = Input.acceleration.x;
+		float moveVertical = Input.acceleration.y;
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce (movement * speed);	
+	}
+
+	void OnTriggerEnter (Collider other)
+	{	
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+			count += 1;
+			setCountText ();
+		}			
+	}
+
+	void setCountText () 
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 4) {
+			winText.text = "You Win!";
+		}
+	}
+}
